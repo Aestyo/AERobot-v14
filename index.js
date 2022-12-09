037f7ef100b39ceba669880b17fad57cdf825211
Mathis Sauvage <sauvagemathis@gmail.com>
2022-12-09 11:18:14 +0100

    :sparkles: Handlers
    Added commands and events handler and 2 events

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
 const dotenv = require('dotenv');
 dotenv.config();
 
 // Création de l'instance du client
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers ] });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./src/commands/${file}`);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at "./src/commands/${file}" is missing a required "data" or "execute" property.`);
+	}
+}
+
+const eventFiles = fs.readdirSync('./src/events/').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./src/events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
 // Log in to Discord with your client's token
 client.login(process.env.TOKEN_DISCORD);
